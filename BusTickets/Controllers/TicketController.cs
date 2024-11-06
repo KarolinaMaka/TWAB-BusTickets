8e355da0228a61a6de673914b4e2870382913385
wiktoriamrozek <wiktoria.mrzek@gmail.com>
2024-11-06 13:08:40 +0100

    Dodanien

---
+++ BusTickets/Controllers/TicketController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using BusTickets.Data;
+using System.Linq;
+
+namespace BusTickets.Controllers
+{
+    public class TicketController : Controller
+    {
+        private readonly TicketDbContext _context;
+
+        public TicketController(TicketDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var tickets = _context.TicketSet.ToList();
+            return View(tickets);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Ticket ticket)
+        {
+            if (ModelState.IsValid)
+            {
+                ticket.PurchaseDate = DateTime.Now;
+                _context.TicketSet.Add(ticket);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(ticket);
+        }
+    }
+}
--- BusTickets/Data/TicketDbContext.cs
+++ BusTickets/Data/TicketDbContext.cs
@@ -1,17 +1,22 @@
 ﻿using Microsoft.EntityFrameworkCore;
 
-
 namespace BusTickets.Data
 {
     public class TicketDbContext : DbContext
     {
+        public TicketDbContext(DbContextOptions<TicketDbContext> options)
+            : base(options)
+        {
+        }
+
         public DbSet<Ticket> TicketSet { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer("Server=KAROLINA-LAPTOP;Database=TicketDatabase;Trusted_Connection=True;TrustServerCertificate=True;");
-
-            // Update this with your actual connection string
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Server=DESKTOP-SN9QJSB;Database=TicketDatabase;Trusted_Connection=True;TrustServerCertificate=True;");
+            }
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
@@ -20,6 +25,5 @@ namespace BusTickets.Data
             modelBuilder.Entity<Ticket>().Property(t => t.BuyerName).HasMaxLength(15);
             modelBuilder.Entity<Ticket>().Property(t => t.BuyerLastName).HasMaxLength(25);
         }
-
     }
 }
